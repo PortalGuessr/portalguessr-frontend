37f2295849d90ae691119a34cab65865c6e19f77
Quddus Salam <quddussalam555@gmail.com>
2024-01-14 11:23:48 +0700

    Fixed the issue with the app unable to build

--- src/main.tsx
+++ src/main.tsx
@@ -10,24 +10,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import "./styles/css/main.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 // ! Bootstrap's breakpoints. For development only!
 // import "./styles/css/css-breakpoints.css";
 
-const store = configureStore({
-  reducer: {},
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
